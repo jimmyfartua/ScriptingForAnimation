bc1cc68486307228c70e7c077e3bd4dab8894dfa
Unknown <jimmyfartua@gmail.com>
2018-11-04 22:11:55 -0700

    game updates

---
+++ Scripting AI/Assets/Scripts/SetCountDown.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCountDown : MonoBehaviour
+{
+	private GameM GMS;
+
+	public void SetCountDownNow()
+	{
+		GMS = GameObject.Find("GameManager").GetComponent<GameM>();
+		GMS.counterDownDone = true;
+	}
+
+
+
+}
